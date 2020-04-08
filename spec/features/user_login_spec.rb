268e0916dc083b99ce0329427e93ba9a46e16179
forgehe <liao098@gmail.com>
2020-04-08 00:28:06 -0700

    user register tests

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -11,11 +11,24 @@ RSpec.feature "Visitor navigates to home page, and registers a new account and l
     visit root_path
     find('ul.navbar-right').click_on 'Signup'
 
+    sleep(0.5)
+    fill_in 'First Name', with: 'John'
+    fill_in 'Last Name', with: 'Smith'
+    fill_in 'Email', with: 'JohnSmith@JohnSmith.com'
+    fill_in 'Password', with: 'JohnSmith'
+    fill_in 'Enter Your Password Again', with: 'JohnSmith'
+    within("form") {
+      click_button
+    }
+      # DEBUG 
     sleep(1)
-    # DEBUG 
     save_screenshot
 
     # VERIFY
+    within('.navbar-right') {
+      expect(page).to have_text 'Signed in as John'
+      expect(page).to have_link 'Logout'
+    }
   end
 
 end
