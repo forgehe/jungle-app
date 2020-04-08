5f3b0b786406e6547c81824ba33d4743d73daffd
forgehe <liao098@gmail.com>
2020-04-08 00:18:25 -0700

    update login page

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -4,22 +4,13 @@ RSpec.feature "Visitor navigates to home page, and registers a new account and l
 
   # SETUP
   before :each do
-    @category = Category.create! name: 'Apparel'
-
-    10.times do |n|
-      @category.products.create!(
-        name:  Faker::Hipster.sentence(3),
-        description: Faker::Hipster.paragraph(4),
-        image: open_asset('apparel1.jpg'),
-        quantity: 10,
-        price: 64.99
-      )
-    end
   end
 
   scenario "Homepage => Login Page => Register" do
     # ACT
     visit root_path
+    find('ul.navbar-right').click_on 'Signup'
+
     sleep(1)
     # DEBUG 
     save_screenshot
